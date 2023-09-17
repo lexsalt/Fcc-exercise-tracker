e41cbd9d943b504f716c4e230c68a4c4d848d73d
emilianosal <emilianosal@hotmail.com>
2023-09-17 13:05:41 +0200

    Passed test

--- index.js
+++ index.js
@@ -139,4 +139,4 @@ app.get("/api/users/:_id/logs", async (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
-//console.log(process.env.MONGO_URI)
+
